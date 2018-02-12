d1a68743e45b0bcf6ee1de7bc7249209431493d9
diegomdo <diego76@gmail.com>
2018-02-12 16:25:21 -0500

    Changes during class
    edits made while watching video

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,10 +5,20 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      Hello!
+      <h1 [innerHTML]="title"></h1>
+      <h1>{{ title }} </h1>
+      <img [src]="logo" width="104" height="97">
+      <input type="text" [value]="name">
+      <div>{{ name }}</div>
     </div>
   `
 })
 export class AppComponent {
+  title: string;
+  logo: string = 'img/cartman.svg';
+  name: string = 'Todd';
 
+  constructor() {
+    this.title = 'Ultimate Angular';
+  }
 }
